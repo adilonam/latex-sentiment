fd5ad6639a9bb43f19327df70b7908954b135750
Adil ABBADI <adil.abbadi.1996@gmail.com>
2025-05-28 10:57:31 +0100

    arch project

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 rm -f main.aux main.toc main.lof main.lot main.log main.fls main.fdb_latexmk main.out
 
-latexmk -C main.tex && latexmk -pvc -pdf main.tex
+latexmk -C main.tex && latexmk -pvc -pdf -shell-escape main.tex
 pdflatex --shell-escape myfile.tex
 pdflatex main.tex
