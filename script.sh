3b24c443368e522c906b3581963a68e21fc5cf3d
Adil ABBADI <adil.abbadi.1996@gmail.com>
2025-06-03 10:49:20 +0100

    Enhance Sprint 2 documentation and implementation for Selenium web scraping and data preprocessing, including detailed use case diagrams and refined scripts for improved functionality and clarity.

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 rm -f main.aux main.toc main.lof main.lot main.log main.fls main.fdb_latexmk main.out
 
-latexmk -C main.tex && latexmk -pvc -pdf -shell-escape main.tex
-pdflatex --shell-escape myfile.tex
+latexmk -C main.tex && latexmk -pvc -pdf main.tex
+latexmk -pvc -pdf main.tex
 pdflatex main.tex
